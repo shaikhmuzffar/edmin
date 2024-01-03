a396a9b2e48677b62cec3e1ce854c01edad83363
muzzu <muzzueducation@gmail.com>
2024-01-03 12:51:05 +0530

    package jason updated and node modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -9,6 +11,13 @@ import { SidbarComponent } from './components/sidbar/sidbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TableComponent } from './components/table/table.component';
 import { SurveyComponent } from './components/survey/survey.component';
+
+// extra modules
+import { ReactiveFormsModule } from '@angular/forms';
+import { DxDataGridModule } from 'devextreme-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +32,12 @@ import { SurveyComponent } from './components/survey/survey.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+
+    DxDataGridModule,
+    HttpClientModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
