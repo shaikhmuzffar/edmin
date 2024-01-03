6e2596ec1823589aed5b23fb2802cb9f37711bb4
muzzu <muzzueducation@gmail.com>
2024-01-03 12:27:47 +0530

    converted components !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { SidbarComponent } from './components/sidbar/sidbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
+import { SurveyComponent } from './components/survey/survey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WrapperComponent,
+    SidbarComponent,
+    DashboardComponent,
+    TableComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule,
